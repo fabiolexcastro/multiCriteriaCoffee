204d6baa0924e2fe96baf4a08281db130d040bba
fabiolexcastro <fabioalexandercastro@gmail.com>
2026-06-12 12:44:33 -0500

    'update'

---
+++ 03_overlaySuits.R
@@ -0,0 +1,14 @@
+
+
+# Install libraries -------------------------------------------------------
+library(pacman)
+p_load(terra, fs, sf, glue, tidyverse, ggspatial, geodata, RColorBrewer)
+
+g <- gc(reset = T)
+rm(list = ls())
+options(scipen = 999, warn = -1)
+
+# Data --------------------------------------------------------------------
+
+
+
